9c9527ddf8dfb588e3d47ad16a55e5d5f003a43a
axel.alonso.contractor <axel.alonso.contractor@bbva.com>
2019-04-25 12:37:28 +0200

    add playoff matches

--- src/app/typography/typography.component.ts
+++ src/app/typography/typography.component.ts
@@ -51,7 +51,7 @@ export class TypographyComponent{
         });
         for (let i = 0; i < finalTableMatches.length; i++) {
             if ((this.getTournamentById(finalTableMatches[i].tournament).name == 'Primera' ||
-               this.getTournamentById(finalTableMatches[i].tournament).name == 'Segunda') && finalTableMatches[i].round > 14) {
+               this.getTournamentById(finalTableMatches[i].tournament).name == 'Segunda') && finalTableMatches[i].round > 18) {
                  finalTableMatches.splice(i, 1);
                  i--;
             }
